c7ba78e26ea5cdf58d9bcc7fc58526cd9638c363
YoshiCity96 <40843184+YoshiCity96@users.noreply.github.com>
2021-03-18 07:51:06 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -30,6 +30,42 @@ $(document).ready(function(){
 	
 });
 
+$('.footer__body').each(function(){
+	var img = $(this);
+	var imgParent = $(this).parent();
+	function parallaxImg () {
+	  var speed = img.data('speed');
+	  var imgY = imgParent.offset().top;
+	  var winY = $(this).scrollTop();
+	  var winH = $(this).height();
+	  var parentH = imgParent.innerHeight();
+ 
+ 
+	  // The next pixel to show on screen      
+	  var winBottom = winY + winH;
+ 
+	  // If block is shown on screen
+	  if (winBottom > imgY && winY < imgY + parentH) {
+		 // Number of pixels shown after block appear
+		 var imgBottom = ((winBottom - imgY) * speed);
+		 // Max number of pixels until block disappear
+		 var imgTop = winH + parentH;
+		 // Porcentage between start showing until disappearing
+		 var imgPercent = ((imgBottom / imgTop) * 100) + (0 - (speed * 50));
+	  }
+	  img.css({
+		 top: imgPercent + '%'
+	  });
+	}
+	$(document).on({
+	  scroll: function () {
+		 parallaxImg();
+	  }, ready: function () {
+		 parallaxImg();
+	  }
+	});
+ });
+
 function themeBlack(){
 	const toggleThemeBlack = document.querySelector('.theme__black')
 	let el = document.documentElement
