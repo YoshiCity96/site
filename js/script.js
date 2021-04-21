0214f5d48f586ba766280b6124ddc1fcefca64c9
YoshiCity96 <40843184+YoshiCity96@users.noreply.github.com>
2021-04-21 20:00:28 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11778,21 +11778,24 @@ new Swiper('.swiper-container',{
 
 
 $('.footer__body').each(function(){
+	
 	var img = $(this);
 	var imgParent = $(this).parent();
 	function parallaxImg () {
+		
 	  var speed = img.data('speed');
 	  var imgY = imgParent.offset().top;
 	  var winY = $(this).scrollTop();
 	  var winH = $(this).height();
 	  var parentH = imgParent.innerHeight();
- 
+		var parentW = imgParent.innerWidth();
  
 	  // The next pixel to show on screen      
 	  var winBottom = winY + winH;
  
 	  // If block is shown on screen
-	  if (winBottom > imgY && winY < imgY + parentH) {
+	
+	  if (winBottom > imgY && winY < imgY + parentH && parentW > 992 ) {
 		 // Number of pixels shown after block appear
 		 var imgBottom = ((winBottom - imgY) * speed);
 		 // Max number of pixels until block disappear
