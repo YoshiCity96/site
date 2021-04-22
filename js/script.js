2e6e0bf597b1da3603286e59d8ffe46306e56cb3
YoshiCity96 <40843184+YoshiCity96@users.noreply.github.com>
2021-04-22 18:29:17 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -11496,33 +11496,44 @@ document.addEventListener('DOMContentLoaded',function(){
 	function formValidate(form) {
 		let error = 0;
 		let formReq = document.querySelectorAll('._req');
-
 		for(let index = 0; index < formReq.length; index++) {
 			const input = formReq[index];
 			formRemoveError(input);
-
+			
+			
 			if(input.classList.contains('_email')){
 				if(emailTest(input)){
 					formAddError(input);
 					error++;
+					
 				}
 			} 
 			else if(input.getAttribute("type") === "checkbox" && input.checked === false){
 				formAddError(input);
 				error++;
+				
 			} 
+			
 			else {
 					if(input.value ===''){
 						formAddError(input);
 						error++;
+						
 					}
 				}
-			
+					
 		}
+		
 		return error;
 	}
 
+	
+		
+		
+	
+
 	function formAddError(input) {
+		
 		input.parentElement.classList.add('_error');
 		input.classList.add('_error');
 	}
@@ -11530,6 +11541,7 @@ document.addEventListener('DOMContentLoaded',function(){
 	function formRemoveError(input) {
 		input.parentElement.classList.remove('_error');
 		input.classList.remove('_error');
+		
 	}
 	
 	function emailTest(input){
